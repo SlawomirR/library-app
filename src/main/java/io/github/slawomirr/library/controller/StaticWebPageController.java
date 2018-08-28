7ee4df204e36f422529bcff5d59fb5f5fc2dd23d
SlawomirR <no.for.spam@wp.pl>
2018-08-28 16:36:27 +0200

    Created Heroku version and deploy.

--- src/main/java/io/github/slawomirr/library/controller/StaticWebPageController.java
+++ src/main/java/io/github/slawomirr/library/controller/StaticWebPageController.java
@@ -10,10 +10,18 @@ public class StaticWebPageController {
 
     @RequestMapping("/")
     public String index(Map<String, Object> model) {
+/*
+        // local version
         model.put("url_info_documentation", "http://localhost:8085/swagger-ui.html");
         model.put("url_link_documentation", "http://localhost:8085/swagger-ui.html");
         model.put("url_info_h2", "http://localhost:8085/console");
         model.put("url_link_h2", "http://localhost:8085/console");
+        model.put("h2_url_field_info", "jdbc:h2:mem:AZ");
+*/
+        model.put("url_info_documentation", "https://calm-dusk-18522.herokuapp.com/swagger-ui.html");
+        model.put("url_link_documentation", "https://calm-dusk-18522.herokuapp.com/swagger-ui.html");
+        model.put("url_info_h2", "https://calm-dusk-18522.herokuapp.com/console");
+        model.put("url_link_h2", "https://calm-dusk-18522.herokuapp.com/console");
         model.put("h2_url_field_info", "jdbc:h2:mem:AZ");
         return "index";
     }
